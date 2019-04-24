887b9f4f0be5dec2832f5e17595e01c2e4b1c270
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-04-24 10:32:23 +0800

    fix typo and rewrite part of c style code into c++ style. add multimap example.

--- cpp-util/test.cpp
+++ cpp-util/test.cpp
@@ -20,7 +20,7 @@ TEST(priority_queue, max_or_min_heap)
 {
     //create an pq and initialize
 
-    vector<int> arr{ 7, 4, 3, 5, 6, 1, 2 };
+    vector<int> arr{7, 4, 3, 5, 6, 1, 2};
     //default max heap
     priority_queue<int> pq(arr.begin(), arr.end());
     EXPECT_EQ(pq.top(), 7);
@@ -36,13 +36,13 @@ TEST(priority_queue, max_or_min_heap)
 /// see https://zh.cppreference.com/w/cpp/algorithm/make_heap
 TEST(priority_queue, using_array_with_method)
 {
-    vector<int> arr{ 7, 4, 3, 5, 6, 1, 2 };
+    vector<int> arr{7, 4, 3, 5, 6, 1, 2};
 
     //max heap
     make_heap(arr.begin(), arr.end());
     EXPECT_EQ(7, arr[0]);
     // also max heap
-    vector<int> arr_{ 1, 2, 3, 4, 5, 6 };
+    vector<int> arr_{1, 2, 3, 4, 5, 6};
     make_heap(arr_.begin(), arr_.end(), less<int>());
     EXPECT_EQ(6, arr_[0]);
 
@@ -59,15 +59,15 @@ TEST(priority_queue, using_array_with_method)
 TEST(SortStruct, lambda)
 {
     //node in node.h
-    //use lambda in C++ to define rule for sort templete.
+    //use lambda in C++ to define rule for sort template.
     vector<node> arr;
 
     arr.push_back(node(3, 1));
     arr.push_back(node(3, 2));
     arr.push_back(node(7, 3));
-    sort(arr.begin(), arr.end(), [](const node & a, const node & b) { return a.c > b.c; });
+    sort(arr.begin(), arr.end(), [](const node& a, const node& b) { return a.c > b.c; });
     EXPECT_EQ(7, arr[0].a);
-    sort(arr.begin(), arr.end(), [](const node & a, const node & b) { return a.c < b.c; });
+    sort(arr.begin(), arr.end(), [](const node& a, const node& b) { return a.c < b.c; });
     EXPECT_EQ(2, arr[0].b);
 }
 
@@ -103,9 +103,9 @@ TEST(math, round)
     EXPECT_EQ(-5, round(-5.444));
     EXPECT_EQ(-5, round(-4.5));
 
-    //for negative float numbers ,just abandom decimal part.
-    EXPECT_EQ(-4, (int)-4.6);
-    EXPECT_EQ(-4, (int)-4.3);
+    //for negative float numbers ,just abandon decimal part.
+    EXPECT_EQ(-4, static_cast<int>(-4.6));
+    EXPECT_EQ(-4, static_cast<int>(-4.3));
 
     //floor 
     EXPECT_EQ(-2, floor(-1.1));
@@ -118,7 +118,7 @@ TEST(math, round)
 // https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
 
 //reverse hashtable.
-//see https://stackoverflow.com/questions/8483985/obtaining-list-of-keys-and-values-from-unordered-map also
+//see https://stackoverflow.com/questions/8483985/obtaining-list-of-keys-and-values-from-unordered-map
 TEST(unordered_map, traverse)
 {
     unordered_map<int, int> map;
@@ -130,17 +130,43 @@ TEST(unordered_map, traverse)
     }
 }
 
+/// multi_map
+/// see https://docs.microsoft.com/en-us/cpp/standard-library/unordered-multimap-class?view=vs-2019#equal_range
+TEST(unordeded_map, mlutimap)
+{
+    std::unordered_multimap<int, char> map;
+    map.insert(std::unordered_multimap<int, char>::value_type(1, 'a'));
+    map.insert(std::unordered_multimap<int, char>::value_type(1, 'b'));
+    map.insert(std::unordered_multimap<int, char>::value_type(2, 'a'));
+    map.insert(std::unordered_multimap<int, char>::value_type(3, 'c'));
+
+    auto it = map.equal_range(1); // pair of iterator first->begin , second->end
+    EXPECT_EQ(it.first->first, 1);
+    EXPECT_EQ(it.first->second, 'a');
+    ++it.first;
+    EXPECT_EQ(it.first->first, 1);
+    EXPECT_EQ(it.first->second, 'b');
+    ++it.first;
+    EXPECT_EQ(it.first, it.second);
+
+    it = map.equal_range(3); //another example.
+    EXPECT_EQ(it.first->first, 3);
+    EXPECT_EQ(it.first->second, 'c');
+    ++it.first;
+    EXPECT_EQ(it.first, it.second);
+}
+
 //Set
 // see https://stackoverflow.com/questions/12850927/adding-elements-of-a-vector-to-an-unordered-set
 TEST(unordered_set, vector2set)
 {
     //build an set from vector.
-    std::vector<int> v{ 1, 1, 2, 3 };
+    std::vector<int> v{1, 1, 2, 3};
     std::unordered_set<int> s(v.begin(), v.end());
     EXPECT_EQ(s.size(), 3);
 
     //copy_function
-    std::vector<int> v_{ 1, 1, 2, 2, 3, 3, 3 };
+    std::vector<int> v_{1, 1, 2, 2, 3, 3, 3};
     std::unordered_set<int> s_;
     std::copy(v_.begin(), v_.end(), std::inserter(s_, s_.end()));
     EXPECT_EQ(s_.size(), 3);
@@ -155,7 +181,7 @@ TEST(vector, set2vector)
     {
         set.insert(i);
     }
-    vector<int> res(set.begin(), set.end());
+    const vector<int> res(set.begin(), set.end());
     EXPECT_EQ(3, res.size());
     vector<int> res_;
     res_.insert(res_.end(), set.begin(), set.end());
